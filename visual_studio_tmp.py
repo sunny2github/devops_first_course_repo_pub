c78c8f7b897fe4be6fcc93300d749055cd59ebc7
sunday <asunny4real@gmail.com>
2026-03-11 19:50:01 +0530

    feat: added a file, installed  MicroSoft python extension and modified the file from visual studio

--- visual_studio_tmp.py
+++ visual_studio_tmp.py
@@ -1,2 +1,6 @@
-Some sample comment 1
-Some sample comment 1
+
+print("Hello, World!")
+
+tasks = ["Check emails", "Team meeting", "Code review"]
+tasks.append("Write report")
+print("Today's tasks:", tasks)
